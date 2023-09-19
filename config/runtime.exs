15b423eef277d2a8a8789d0e9f32a45ef4380f4f
Wendell Richards <wrichards@Wendells-MacBook-Pro.local>
2023-09-19 12:32:17 -0500

    Updated config

--- config/runtime.exs
+++ config/runtime.exs
@@ -54,14 +54,16 @@ if config_env() == :prod do
   config :fc_client, FcClientWeb.Endpoint,
     url: [host: host, port: 443, scheme: "https"],
     http: [
-      # Enable IPv6 and bind on all interfaces.
-      # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
-      # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
-      # for details about using IPv6 vs IPv4 and loopback vs public addresses.
-      ip: {0, 0, 0, 0, 0, 0, 0, 0},
+      transport_options: [socket_opts: [:inet6]],
       port: port
     ],
-    secret_key_base: secret_key_base
+    secret_key_base: secret_key_base,
+    check_origin: [
+      "https://feliciascleaners.com",
+      "https://www.feliciascleaners.com",
+      "http://www.feliciascleaners.com:#{port}",
+      "http://feliciascleaners.com:#{port}"
+    ]
 
   # ## SSL Support
   #
